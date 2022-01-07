4a1f808bb1ceddd46f109ba8263a1ea455bc3806
kamia700 <kaana757@gmail.com>
2022-01-07 20:03:16 +0100

    added sign out option

--- src/components/header/header.container.jsx
+++ src/components/header/header.container.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
+import {auth} from '../../farebase/firebase.utils';
 import './header.styles.scss'
 
-const Header = () => (
+const Header = ({currentUser}) => (
   <div className='header'>
     <Link to='/'>
       <Logo className='logo' />
@@ -11,7 +12,12 @@ const Header = () => (
     <div className='options'>
       <Link className='option' to='/shop'>Shop</Link>
       <Link className='option' to='/shop'>Contact</Link>
-      <Link className='option' to='/signin'>Sign In</Link>
+      {
+        currentUser ?
+        <div className='option' onClick={() => auth.signOut()}>Sign Out</div>
+        :
+        <Link className='option' to='/signin'>Sign In</Link>
+      }
     </div>
   </div>
 );
