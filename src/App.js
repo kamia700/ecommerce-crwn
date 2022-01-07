21531ed2e0a8c3cf9cee73fc80430ab4a81f8d8d
kamia700 <kaana757@gmail.com>
2022-01-07 19:21:03 +0100

    listening to authentication state changes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,30 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.container';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './farebase/firebase.utils'
 
-function App() {
-  return (
+class App extends React.Component {
+  constructor () {
+    super();
+    this.state = {
+      currentUser: null
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount () {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=> {
+      this.setState({ currentUser: user });
+    })
+  }
+
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
+   
+  render () {
+    return (
     <div>
       <Header />
       <Switch>
@@ -17,7 +38,7 @@ function App() {
         <Route path='/signin' component ={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
-  );
+  )};
 };
 
 export default App;
