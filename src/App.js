6d9f391c8c1ee1f25c4df5247d000c08f10f7f6a
kamia700 <kaana757@gmail.com>
2022-01-08 13:22:46 +0100

    storing user data in firstore db

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.container';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './farebase/firebase.utils'
+import { auth, createUserProfileDocument } from './farebase/firebase.utils'
 
 class App extends React.Component {
   constructor () {
@@ -19,8 +19,10 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount () {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user=> {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      this.setState({ currentUser: userAuth });
+
+      createUserProfileDocument(userAuth);
     })
   }
 
