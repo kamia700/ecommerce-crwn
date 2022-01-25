0fd2f4298074ed633bce68ee54d847619fabdad2
kamia700 <kaana757@gmail.com>
2022-01-25 15:09:46 +0100

    removed items from cart

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -14,11 +14,18 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         hidden: !state.hidden
       }
     case CartActionTypes.ADD_ITEM:
-      console.log(state.cartItems)
+      // console.log(state.cartItems)
       return {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
     }  
+    case CartActionTypes.CLEAR_ITEM_FROM_CART:
+      return {
+        ...state,
+        cartItems: state.cartItems.filter(
+          cartItem => cartItem.id !== action.payload.id
+        )
+    };
     default:
       return state;
   }
